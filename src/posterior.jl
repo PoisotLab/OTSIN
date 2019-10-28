d1b52b575c3eb25bae7153f89cd39446dc099e9b
michielstock <michielfmstock@gmail.com>
2019-10-28 13:43:53 +0100

    :tada: fit posterior

--- src/posterior.jl
+++ src/posterior.jl
@@ -1,29 +1,25 @@
-
 using Distributions: MultivariateNormal
 
-function optimaltransport_(M::AbstractMatrix, a::AbstractVector, b::AbstractVector;
-                        λ::Real=1.0, maxitter::Int=100, ϵ::Real=1e-5)
+
+function optimaltransport_safe(M::AbstractMatrix, a::AbstractVector, b::AbstractVector;
+                        λ::Real=1.0, maxitter::Int=10, ϵ::Real=1e-5)
     Q = exp.(λ * M)
     iter = 0
     while iter < maxitter && maximum(abs.(sum(Q, dims=2)[:] - a)) > ϵ
         iter += 1
-        Q = Diagonal(a ./ sum(Q, dims=2)[:]) * Q
-        Q = Q * Diagonal(b ./ sum(Q, dims=1))
+        Q = a .* (sum(Q, dims=2) .\ Q)
+        Q = Q .* transpose(b) ./ sum(Q, dims=1)
     end
     return Q
 end
 
-
-function laplace_posterior(M̂, P, γ=1e-2,
-                reg::Regularization=L2(),
-                maxitter::Int=20,
-                ϵ::Real=1e-4)
+function laplace_posterior(M, P; γ=1e-2,
+                maxitter::Int=20, tol=1e-4)
     a, b = marginals(P)
-    Q = M -> Q = optimaltransport_(M, a, b,
-                    ϵ=ϵ, maxitter=maxitter)
-    l = M -> KL(P, Q(M)) + γ * r(M, reg)
-    hess = Zygote.hessian(l, M̂)
-    return MultivariateNormal(vec(M̂), hess^-1)
+    Q = M -> Q = optimaltransport_safe(exp.(M), a, b)
+    l = M -> relative_entropy(P, Q(M)) + γ * sum(M .* M)
+    hess = Zygote.hessian(l, M)
+    return MultivariateNormal(vec(exp.(M)), Symmetric(hess+tol*I)^-1)
 end
 
 #TODO: test and explore this.
